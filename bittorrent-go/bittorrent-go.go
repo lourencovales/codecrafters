0fb7ac9f4030c342df7666daa993bbd48360a78a
Lourenço Vales <133565059+lourencovales@users.noreply.github.com>
2025-06-12 15:48:59 +0200

    We now have an info() function
    Useful for extracting all the information from the torrent file

--- bittorrent-go/bittorrent-go.go
+++ bittorrent-go/bittorrent-go.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -12,7 +15,7 @@ import (
 // all the information needed through the execution of the program
 type decoder struct {
 	command string
-	args    string
+	args    []byte
 	pos     int
 }
 
@@ -22,16 +25,16 @@ func main() {
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	// TODO: this logic needs to get out of main, probably
-	decoder := &decoder{}
+	d := &decoder{}
 
-	decoder.command = os.Args[1]
+	d.command = os.Args[1]
 
-	if decoder.command == "decode" {
+	if d.command == "decode" {
 
-		decoder.args = os.Args[2]
-		decoder.pos = 0
+		d.args = []byte(os.Args[2])
+		d.pos = 0
 
-		decoded, err := decoder.decodeBencode()
+		decoded, err := d.decodeBencode()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -43,8 +46,30 @@ func main() {
 		}
 
 		fmt.Println(string(marsh))
+	} else if d.command == "info" {
+		d.args = []byte(os.Args[2])
+
+		decoded, err := d.info()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		marsh, err := json.Marshal(decoded) // answer needs to be in json
+		if err != nil {
+			fmt.Println(fmt.Errorf("error marshalling json"))
+		}
+
+		fmt.Println(string(marsh))
+
+		//s, ok := decoded.(string)
+		//if !ok {
+		//	fmt.Errorf("problem with type assertion")
+		//}
+
+		//fmt.Println(marsh)
 	} else {
-		fmt.Println("Unknown command: " + decoder.command)
+		fmt.Println("Unknown command: " + d.command)
 		os.Exit(1)
 	}
 }
@@ -52,48 +77,97 @@ func main() {
 // The decodeBencode function is the main point of entry for data, where we
 // select which type of bencoded data we will be dealing with, and dispatch it
 // accordingly
-func (decoder *decoder) decodeBencode() (interface{}, error) {
+func (d *decoder) decodeBencode() (interface{}, error) {
 
-	if decoder.pos >= len(decoder.args) { // out-of-bounds check
+	if d.pos >= len(d.args) { // out-of-bounds check
 		return nil, fmt.Errorf("unexpected end of input")
 	}
 
 	// the first character determines which type of data we're dealing with.
 	// this will get updated as we progress through the input
-	firstChar := rune(decoder.args[decoder.pos])
+	firstChar := rune(d.args[d.pos])
 	if unicode.IsDigit(firstChar) {
-		return decoder.decodeString()
+		return d.decodeString()
 	}
 	if firstChar == 'i' {
-		return decoder.decodeIntegers()
+		return d.decodeIntegers()
 	}
 	if firstChar == 'l' {
-		return decoder.decodeList()
+		return d.decodeList()
 	}
 	if firstChar == 'd' {
-		return decoder.decodeDict()
+		return d.decodeDict()
 	}
-	return "", fmt.Errorf("unsupported bencode: %d, %c", decoder.pos, firstChar)
+	return "", fmt.Errorf("unsupported bencode: %d, %c", d.pos, firstChar)
+}
+
+func (d *decoder) info() (interface{}, error) {
+
+	// file ingestion
+	file, err := os.ReadFile(string(d.args))
+	if err != nil {
+		return nil, err
+	}
+
+	// need to figure out where the info dict is
+	start := bytes.Index(file, []byte("4:info"))
+	if start == -1 {
+		return nil, fmt.Errorf("info dict not found")
+	}
+	start += len("4:info")
+
+	// decode just that data and calculate the hash
+	subDec := &decoder{args: file, pos: start}
+	_, err = subDec.decodeDict()
+	if err != nil {
+		return nil, err
+	}
+	end := subDec.pos
+
+	infoHash := sha1.Sum(file[start:end])
+	hashHex := hex.EncodeToString(infoHash[:])
+
+	// for URL and Length params, make use of the already existing functions and
+	// parse the results with a bit of type assertion magic
+	d.args = file
+
+	decoded, err := d.decodeDict()
+	if err != nil {
+		return nil, err
+	}
+
+	subMap, ok := decoded["info"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("problem with type assertion")
+	}
+
+	return fmt.Sprintf(
+		"Tracker URL: %s\nLength: %d\n, Info Hash: %s\n",
+		decoded["announce"],
+		subMap["length"],
+		hashHex,
+	), nil
+
 }
 
 // The decodeString function deals with bencoded strings
-func (decoder *decoder) decodeString() (string, error) {
+func (d *decoder) decodeString() (string, error) {
 
 	var firstColonIndex int
 
 	// we detect and save where the first colon is, as this is the delimiter
 	// for bencoded strings
-	for i := decoder.pos; i < len(decoder.args); i++ {
-		if decoder.args[i] == ':' {
+	for i := d.pos; i < len(d.args); i++ {
+		if d.args[i] == ':' {
 			firstColonIndex = i
 			break
 		}
 	}
 
 	// the string we want to extract
-	lengthStr := decoder.args[decoder.pos:firstColonIndex]
+	lengthStr := d.args[d.pos:firstColonIndex]
 	// this is to extract the integer that codifies the string
-	length, err := strconv.Atoi(lengthStr)
+	length, err := strconv.Atoi(string(lengthStr))
 	if err != nil {
 		return "", err
 	}
@@ -101,57 +175,57 @@ func (decoder *decoder) decodeString() (string, error) {
 	// this is the full size of the string
 	size := firstColonIndex + length + 1
 
-	decodedString := decoder.args[firstColonIndex+1 : size]
+	decodedString := d.args[firstColonIndex+1 : size]
 
 	// the pos argument is updated with the full size of the string
-	decoder.pos = size
+	d.pos = size
 
-	return decodedString, nil
+	return string(decodedString), nil
 }
 
 // The decodeIntegers function deals with bencoded integers
-func (decoder *decoder) decodeIntegers() (int, error) {
+func (d *decoder) decodeIntegers() (int, error) {
 
 	// We start one char to the right to ignore the delimiter
-	start := decoder.pos + 1
+	start := d.pos + 1
 	end := start
 
 	// we detect and save where the end of the integer is
-	for ; end < len(decoder.args); end++ {
-		if decoder.args[end] == 'e' {
+	for ; end < len(d.args); end++ {
+		if d.args[end] == 'e' {
 			break
 		}
 	}
 
-	if end >= len(decoder.args) {
+	if end >= len(d.args) {
 		return 0, fmt.Errorf("malformed integer")
 	}
 
 	// the integer is between the start and end parameters
-	result := decoder.args[start:end]
+	result := d.args[start:end]
 
 	// convert it to int type
-	intResult, err := strconv.Atoi(result)
+	intResult, err := strconv.Atoi(string(result))
 	if err != nil {
 		return 0, err
 	}
 
 	// move the pos argument one to the right so that we ignore the last char
-	decoder.pos = end + 1
+	d.pos = end + 1
 
 	return intResult, nil
 }
 
 // The decodeList function deals with bencoded lists
-func (decoder *decoder) decodeList() ([]interface{}, error) {
+func (d *decoder) decodeList() ([]interface{}, error) {
 
 	result := make([]interface{}, 0)
-	decoder.pos++ // ignoring the first char
+	d.pos++ // ignoring the first char
 
 	// while not out of bounds and we haven't found the char, keep going
-	for decoder.pos < len(decoder.args) && decoder.args[decoder.pos] != 'e' {
+	for d.pos < len(d.args) && d.args[d.pos] != 'e' {
 		// recursively deal with each of the components of the list
-		r, err := decoder.decodeBencode()
+		r, err := d.decodeBencode()
 		if err != nil {
 			return nil, err
 		}
@@ -159,21 +233,21 @@ func (decoder *decoder) decodeList() ([]interface{}, error) {
 	}
 
 	// move the pos argument one to the right so that we ignore the last char
-	decoder.pos++
+	d.pos++
 	return result, nil
 }
 
 // The decodeDict function deals with dictionaries
-func (decoder *decoder) decodeDict() (map[string]interface{}, error) {
+func (d *decoder) decodeDict() (map[string]interface{}, error) {
 
 	result := make(map[string]interface{})
-	decoder.pos++ // ignoring the first char
+	d.pos++ // ignoring the first char
 
 	var lastKey string
 
-	for decoder.pos < len(decoder.args) && decoder.args[decoder.pos] != 'e' {
+	for d.pos < len(d.args) && d.args[d.pos] != 'e' {
 		// Keys must be strings
-		keyRaw, err := decoder.decodeString()
+		keyRaw, err := d.decodeString()
 		if err != nil {
 			return nil, fmt.Errorf("invalid dictionary key: %w", err)
 		}
@@ -184,7 +258,7 @@ func (decoder *decoder) decodeDict() (map[string]interface{}, error) {
 		}
 		lastKey = keyRaw
 
-		value, err := decoder.decodeBencode()
+		value, err := d.decodeBencode()
 		if err != nil {
 			return nil, fmt.Errorf("invalid dictionary value for key %q: %w", keyRaw, err)
 		}
@@ -193,10 +267,10 @@ func (decoder *decoder) decodeDict() (map[string]interface{}, error) {
 	}
 
 	// we're out of bounds or we never found the delimiter, so we error out
-	if decoder.pos >= len(decoder.args) || decoder.args[decoder.pos] != 'e' {
+	if d.pos >= len(d.args) || d.args[d.pos] != 'e' {
 		return nil, fmt.Errorf("malformed dictionary")
 	}
 
-	decoder.pos++ // Skip the delimiter
+	d.pos++ // Skip the delimiter
 	return result, nil
 }
